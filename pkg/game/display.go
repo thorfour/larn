221c7d8afe119233dc6e3de21f721604cd3f3877
thor <thorvald.hansen@omnitier.com>
2017-11-24 18:36:45 -0800

    Added missing info in the infoBar string

--- pkg/game/display.go
+++ pkg/game/display.go
@@ -9,6 +9,6 @@ func (d *Data) world() string {
 
 // infoBar returns the stringified info bar for terminal display
 func (d *Data) infoBar() string {
-	return fmt.Sprintf("Spells: %v( %v) AC: %v WC: %v Level %v Exp: %v %s\n HP: %v( %v) STR=%v INT=%v WIS=%v CON=%v DEX=%v CHA=%v LV: %v Gold: %v",
-		d.spells, d.maxSpells, d.ac, d.wc, d.level, d.exp, d.hp, d.str, d.intelligence, d.wisdom, d.con, d.dex, d.cha, d.loc, d.gold)
+	return fmt.Sprintf("Spells: %v( %v) AC: %v WC: %v Level %v Exp: %v %s\nHP: %v( %v) STR=%v INT=%v WIS=%v CON=%v DEX=%v CHA=%v LV: %v Gold: %v",
+		d.spells, d.maxSpells, d.ac, d.wc, d.level, d.exp, d.title, d.hp, d.maxHP, d.str, d.intelligence, d.wisdom, d.con, d.dex, d.cha, d.loc, d.gold)
 }

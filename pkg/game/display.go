e5f256c644ddf19c6e117f120788eea261bf3398
thor <thorvald.hansen@omnitier.com>
2017-11-24 20:39:00 -0800

    Added generating startZone

--- pkg/game/display.go
+++ pkg/game/display.go
@@ -4,7 +4,12 @@ import "fmt"
 
 // world returns a string representation of the world
 func (d *Data) world() [][]rune {
-	return d.infoBarGrid()
+	grid := startZone()
+	bar := d.infoBarGrid()
+	for i := range bar { // Append the info bar
+		grid = append(grid, bar[i])
+	}
+	return grid
 }
 
 // infoBarGrid returns the info bar in display grid format
@@ -14,4 +19,18 @@ func (d *Data) infoBarGrid() [][]rune {
 	r[1] = []rune(fmt.Sprintf("HP: %v( %v) STR=%v INT=%v WIS=%v CON=%v DEX=%v CHA=%v LV: %v Gold: %v", d.hp, d.maxHP, d.str, d.intelligence, d.wisdom, d.con, d.dex, d.cha, d.loc, d.gold))
 
 	return r
+}
+
+// startZone is the toplevel of the world
+func startZone() [][]rune {
+	grid := make([][]rune, borderHeight)
+	for i := range grid {
+		row := make([]rune, borderWidth)
+		for j := range row {
+			row[j] = '.'
+		}
+		grid[i] = row
+	} // TODO THOR randomly insert start zone locations
+
+	return grid
 }

da15b103420b1e1b9e0f4c9e7c5f29ccab859a75
thor <github@thor-hansen.com>
2018-06-09 13:12:01 -0700

    added bottom lines to bank

--- pkg/game/bank.go
+++ pkg/game/bank.go
@@ -20,6 +20,22 @@ func bankPage(stones []items.Gem) string {
 	w := tabwriter.NewWriter(buf, 5, 0, 1, ' ', tabwriter.TabIndent)
 	fmt.Fprintln(w, "Gemstone\t\t\tAppraisal\t\tGemstone\t\t\tAppraisal")
 
+	// Display all stones
+	for i := range stones { // TODO handle doing two gems on each line
+		fmt.Fprintf(w, "%s\t\t\t%v\t\t", stones[i].String(), 0) // TODO get appraisal value
+	}
+
+	// Padd out the rest
+	for i := 0; i < 16-len(stones); i++ {
+		fmt.Fprintln(w, " \t\t\t \t\t \t\t\t ")
+	}
+
+	// Append helper lines at the bottom
+	fmt.Fprintf(w, "                       You have %v gold pieces in the bank\n", 0) // TODO get bank count
+	fmt.Fprintf(w, "                       You have %v gold pieces\n", 0)             // TODO get gold count
+	fmt.Fprintln(w)
+	fmt.Fprintf(w, "  Your wish? [(d) deposit, (w) withdraw, (s) sell a stone, or escape]")
+
 	w.Flush()
 	return pg + string(buf.Bytes())
 }

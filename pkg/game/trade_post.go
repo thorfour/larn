39a4c60f229734c8ce73818a488e943e2c6fbb9e
thor <github@thor-hansen.com>
2018-07-06 13:47:12 -0500

    Added inventory to trading post printout

--- pkg/game/trade_post.go
+++ pkg/game/trade_post.go
@@ -1,6 +1,12 @@
 package game
 
-import termbox "github.com/nsf/termbox-go"
+import (
+	"bytes"
+	"fmt"
+	"text/tabwriter"
+
+	termbox "github.com/nsf/termbox-go"
+)
 
 func tradingPostWelcome() string {
 	return `
@@ -10,13 +16,24 @@ func tradingPostWelcome() string {
   only 20% of their value were they to be new.  If the items are badly
   damaged, we will pay only 10% of their new value.
 	  
-	  `
+  Here are the items we would be willing to buy from you:
+`
 }
 
 func tradingPost(inv []string) string {
 	s := tradingPostWelcome()
+	buf := bytes.NewBuffer(make([]byte, 100))
+	w := tabwriter.NewWriter(buf, 5, 0, 1, ' ', tabwriter.TabIndent)
+	for i, t := range inv {
+		if i%2 == 0 {
+			fmt.Fprintf(w, "  %s\t\t\t\t", t)
+		} else {
+			fmt.Fprintf(w, "%s\n", t)
+		}
+	}
+	w.Flush()
 
-	return s
+	return s + string(buf.Bytes())
 }
 
 // tradingPostHandler input handler for the trading post
